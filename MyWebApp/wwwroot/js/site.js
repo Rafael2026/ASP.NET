44645eb181408cb0e1457ccac5eae7aa957fe806
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-10-23 17:17:21 +0200

    Minor changes

--- MyWebApp/wwwroot/js/site.js
+++ MyWebApp/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+AddAltToImg = function (imageTagAndImageID, imageContext) {
+  ///<signature>
+  ///<summary> Adds an alt tab to the image
+  // </summary>
+  //<param name="imgElement" type="String">The image selector.</param>
+  //<param name="ContextForImage" type="String">The image context.</param>
+  ///</signature>
+  var imageElement = $(imageTagAndImageID, imageContext);
+  imageElement.attr('alt', imageElement.attr('id').replace(/ID/, ''));
+}
