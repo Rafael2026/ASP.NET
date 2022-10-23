e653a6e5f4e92893020fe633a9203c10e8c49b39
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-10-23 14:23:25 +0200

    Minor changes

--- MyWebApp/scripts/app.js
+++ MyWebApp/scripts/app.js
@@ -1,17 +1,22 @@
 function TSButton() {
-    var name = "Fred";
-    document.getElementById("ts-example").innerHTML = greeter(user);
+  var name = "Fred";
+  document.getElementById("ts-example").innerHTML = greeter(user);
 }
+
 var Student = /** @class */ (function () {
-    function Student(firstName, middleInitial, lastName) {
-        this.firstName = firstName;
-        this.middleInitial = middleInitial;
-        this.lastName = lastName;
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
-    return Student;
+  
+  function Student(firstName, middleInitial, lastName) {
+    this.firstName = firstName;
+    this.middleInitial = middleInitial;
+    this.lastName = lastName;
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+
+  return Student;
 }());
+
 function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+  return "Hello, " + person.firstName + " " + person.lastName;
 }
+
 var user = new Student("Fred", "M.", "Smith");
