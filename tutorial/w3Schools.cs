6052d8de0376a795a4d66a09d32a78326511c594
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-10-26 20:41:25 +0200

    Start c# tutorial

---
+++ tutorial/w3Schools.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      /* The code below will print the words Hello World to the screen, and it is amazing */
+      Console.WriteLine("Hello World!"); // This is a comment
+      Console.WriteLine("I am Learning C#");
+      Console.WriteLine("It is awesome!");
+      Console.WriteLine("3 + 3 = " (3 + 3));
+    }
+  }
+}
