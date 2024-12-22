1e5d424c9dc2736abf09a6a668005f590e06720c
Rafael2026 <rafaaguilarmunoz@gmail.com>
2024-12-22 17:43:20 +0100

    Many project changes and upgrades

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,12 +1,29 @@
+using static Microsoft.AspNetCore.Http.StatusCodes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddRazorPages();
-
+builder.Services.AddRazorPages();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(60);
+    options.ExcludedHosts.Add("example.com");
+    options.ExcludedHosts.Add("www.example.com");
+});
+
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = Status308PermanentRedirect;
+    options.HttpsPort = 5001;
+});
 
 var app = builder.Build();
 
@@ -15,22 +32,19 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
-/* if (!app.Environment.IsDevelopment())
-{
+} else {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
-} */
+}
 
 app.UseHttpsRedirection();
-//app.UseStaticFiles();
+app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapControllers();
-//app.MapRazorPages();
+app.MapRazorPages();
 
 app.Run();
