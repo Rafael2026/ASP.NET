598b9d349891a877deb1ae42f5167122cbdae69d
Rafael2026 <rafaaguilarmunoz@gmail.com>
2025-06-16 19:39:52 +0200

    Fix all issues on the repository

--- MyWebApp/obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
+++ MyWebApp/obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3109eefec3945fe9cc5e166908154e7b2e06cf40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669d36f054fa491f5ce68f578b6dd10251fed879")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
